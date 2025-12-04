4d3070e20d98416658bf8f6722012623bf25d02b
Jared White <jaredtwhite4@gmail.com>
2025-12-04 17:00:26 -0600

    Node created

--- RRT-Node/Node.py
+++ RRT-Node/Node.py
@@ -4,35 +4,37 @@ from rclpy.node import Node
 import RRT_Algorithm as rrt
 import B_Spline as bsp
 import BinaryOccupancyMap
+from std_msgs.msg import Float64MultiArray
 
 class FindPathNode(Node):
     def __init__(self):
-        super().__init__("INSERTNAME")
-        # self.path_publisher_ = self.create_publisher(
-        #     # type
-        #     # topic
-        #     10)
-        self.get_logger().info("")
+        super().__init__("get_path")
+        self.path_publisher_ = self.create_publisher(
+            Float64MultiArray,
+            '/Xpath',
+            10)
+        self.time_publisher_ = self.create_publisher(
+            Float64MultiArray,
+            '/tpath',
+            10)
+        self.timer = self.create_timer(1.0, self.FindPath)
+        self.get_logger().info("FindPathNode has started.")
     
     def FindPath(self, start, goal, binaryMap):
         area = [ [0, len(binaryMap[0])-1], [0, len(binaryMap)-1] ]
         print(area[0][0], area[0][1], area[1][0], area[1][1])
         path, cnct, vertices = rrt.GetPath(binaryMap, area, start, goal)
-        
-        if not cnct:
-            self.get_logger().info("\033[91mNo path found.\033[00m")
-            rrt.ShowMap([], goal, binaryMap, vertices)
-            return None
+
         
         # Smooth path with B-Spline
         res = 50
         smoothPath, tPoints = bsp.GetBSpline(path, res, binaryMap)
         
-        self.get_logger().info("\033[92mPath to goal found.\033[00m")
-        rrt.ShowMap(path, goal, binaryMap, vertices)
-        bsp.ShowBSpline(smoothPath, tPoints, goal, binaryMap, vertices)
+        xpointmsg = PathToMsg(smoothPath)
+        tpointmsg = TimeToMsg(tPoints)
         
-        return smoothPath
+        self.path_publisher_.publish(xpointmsg)
+        self.time_publisher_.publish(tpointmsg)
 
 def FindStart(binaryMap):
     for r in range(len(binaryMap)):
@@ -41,6 +43,20 @@ def FindStart(binaryMap):
                 start = (c, r)
                 return start
     return None
+
+def PathToMsg(path):
+    msg = Float64MultiArray()
+    flat_path = []
+    for point in path:
+        flat_path.append(point[0])
+        flat_path.append(point[1])
+    msg.data = flat_path
+    return msg
+
+def TimeToMsg(tPoints):
+    msg = Float64MultiArray()
+    msg.data = tPoints
+    return msg
 
 def main(args=None):
     rclpy.init(args=args)
